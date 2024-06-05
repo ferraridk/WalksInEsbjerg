aa6814725319045faf6336b2c85f748a49bdb06b
ferraridk <henrikferrari@gmail.com>
2024-06-05 12:59:39 +0200

    Creating DbContext

---
+++ WalksInEsbjerg.API/Data/WalksInEsbjergDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalksInEsbjerg.API.Models.Domain;
+
+namespace WalksInEsbjerg.API.Data
+{
+    public class WalksInEsbjergDbContext: DbContext
+    {
+        public WalksInEsbjergDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
