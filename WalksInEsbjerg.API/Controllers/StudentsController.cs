f7d6bf6ae72d3d1b20755871365b8e3ec01b8fc1
ferraridk <henrikferrari@gmail.com>
2024-06-05 11:58:04 +0200

    Create StudentsController.cs

---
+++ WalksInEsbjerg.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalksInEsbjerg.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "James", "Gert", "Alba", "Mario", "Claus", "Julie" };
+            return Ok(studentNames);
+
+        }
+    }
+}
